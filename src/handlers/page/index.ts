16ba113d51807aa43430500701e7c53d61a0c3d1
kev-techi <kevin@techi.be>
2024-10-19 12:41:31 +0200

    import relative to module

--- src/handlers/page/index.ts
+++ src/handlers/page/index.ts
@@ -41,7 +41,7 @@ export const pageHandler = async (_req: Request): Promise<Response> => {
 
     if (Deno.env.get("DEBUG") === "true") {
       const hmrCode = await Deno.readTextFile(
-        new URL("../../hmr/index.html", import.meta.url)
+        new URL("/src/hmr/index.html", import.meta.dirname)
       );
 
       content += hmrCode;
